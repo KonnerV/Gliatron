38c9b758e9b042ad307abfabf29864d33d718a12
Konner V <94082086+KonnerV@users.noreply.github.com>
2024-07-30 21:39:12 +0100

    Changed some things around and added a compute() function to determine an output y

--- src/nn.c
+++ src/nn.c
@@ -7,17 +7,15 @@
 
 #define ARR_LEN(x) sizeof(x)/sizeof(x[0])
 
-// training data type, just to make data manipulation easier
 typedef struct {
     int x;
     int y;
 } train_data;
 
-
-// Each Neuron has a bias, weight(s), and an activation function
 typedef struct {
     float b;
     float* w;
+    uint16_t n_w;
 } neuron_t;
 
 // Maths stuff
@@ -60,7 +58,7 @@ float rnd_float(void) {
 
 
 void init_neuron(neuron_t* neuron, uint16_t n) {
-    if ((neuron->w = (float*)calloc(n, sizeof(float))) == NULL) {
+    if ((neuron->w = (float*)malloc(n*sizeof(float))) == NULL) {
         printf("Err alloc 3\n");
         exit(-1);
     }
@@ -68,33 +66,49 @@ void init_neuron(neuron_t* neuron, uint16_t n) {
         neuron->w[i] = rnd_float();
     }
     neuron->b = rnd_float();
+    neuron->n_w = n;
     return;
 }
 
-void init_layers(neuron_t** layer, uint16_t  n_neurons) {
-    if ((*layer = (neuron_t*)calloc(n_neurons, sizeof(neuron_t))) == NULL) {
+void init_layers(neuron_t** layer, uint16_t  n_neurons, uint16_t n_neurons_prev) {
+    if ((*layer = (neuron_t*)malloc(n_neurons*sizeof(neuron_t))) == NULL) {
         printf("Err alloc 2\n");
         exit(-1);
     }
     for (size_t i=0;i<n_neurons;++i) {
-        init_neuron(&((*layer)[i]), 1);
+        init_neuron(&((*layer)[i]), n_neurons_prev);
         printf("w: %f, b: %f\n", (*layer)[i].w[0], (*layer)[i].b);
     }
     return;
+}
+
+float compute(neuron_t** nn, uint8_t n_layers, uint8_t n_neurons, float x) {
+    float y=0;
+    for (size_t j=0;j<n_layers;++j) {
+        for (size_t k=0;k<n_neurons;++k) {
+            for (size_t l;l<nn[j][k].n_w;++l) {
+                y += nn[j][k].w[l] * x;    
+            }
+            y += nn[j][k].b;
+        }
+        y = nn_tanh(y);
+    }
+    return y;
 }
 
 int main(int argc, char** argv) {
     neuron_t** nn;
     uint8_t n_layers = 2;
     uint8_t n_neurons = 1;
-    if ((nn = (neuron_t**)calloc(2, sizeof(neuron_t*))) == NULL) {
+    if ((nn = (neuron_t**)malloc(2*sizeof(neuron_t*))) == NULL) {
         printf("Err alloc 1\n");
         exit(-1);
     }
     for (size_t i=0;i<n_layers;++i) {
-        init_layers(&nn[i], n_neurons);
+        init_layers(&nn[i], n_neurons, n_neurons);
     }
 
+    printf("Computed y: %f\n", compute(nn, n_layers, n_neurons, 2.f));
 
     for (size_t j=0;j<n_layers;++j) {
         for (size_t k=0;k<n_neurons;++k) {
