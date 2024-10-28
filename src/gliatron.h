5cfc21a626bb822209ea9acf13bf607af8c6260f
Konner V <94082086+KonnerV@users.noreply.github.com>
2024-10-28 18:21:42 +0000

    misc fixes

--- src/gliatron.h
+++ src/gliatron.h
@@ -27,10 +27,10 @@ double** entrywise_product_matrix(double** m1, double** m2, size_t n_rows, size_
 double rnd_float(void);
 
 // Init
-void init_neuron(neuron_t* neuron, uint16_t n);
-void init_layers(neuron_t** layer, uint16_t  n_neurons, uint16_t n_neurons_prev);
+int8_t init_neuron(neuron_t* neuron, uint16_t n);
+int8_t init_layers(neuron_t** layer, uint16_t  n_neurons, uint16_t n_neurons_prev);
 
 // NN algos
 double** compute(size_t n_layers, uint8_t* n_neurons, double*** w_matrix, double** bias_matrix, double** x_matrix, size_t n_rows, size_t n_cols, double (*activation)(double));
-void grad_desc(neuron_t*** nn, double*** w_m, double** b_m, double** act_m, uint8_t n_layers, uint8_t* n_neurons, double (*act_inv)(double), double (*act_prime)(double), double** x, size_t x_rows, double** y, double lr, size_t n_samples);
+void grad_desc(neuron_t*** nn, double*** w_m, double** act_m, uint8_t n_layers, uint8_t* n_neurons, double (*act_inv)(double), double (*act_prime)(double), double** x, size_t x_rows, double** y, double lr);
 
